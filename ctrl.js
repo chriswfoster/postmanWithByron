3f694e6241a856a53751c58cf0de74916c854dec
Chriswf <chriswf@SpinScis-MacBook-Pro-5.local>
2018-08-14 10:23:24 -0500

    endpoints

---
+++ ctrl.js
@@ -0,0 +1,39 @@
+const usersArray = [
+    {
+        name: "Chriswf",
+        age: 55,
+        password: "iliketurtles"
+    },
+    {
+        name: "Byron",
+        age: 1000,
+        password: "footballiskewl"
+    },
+    {
+        name: "Kiran",
+        age: 30,
+        password: "spinsciiswhereiwork"
+    }
+]
+const getUsers = (req, res) => {
+    res.status(200).json(usersArray)
+}
+const updateUser = (req, res) => {
+    res.status(200).json(usersArray);
+}
+const createUser = (req, res) => {
+    usersArray.push(body)
+    res.status(200).json(usersArray)
+}
+const deleteUser = (req, res) => {
+
+
+    res.status(200).json(usersArray)
+}
+
+module.exports = {
+    getUsers,
+    updateUser,
+    createUser,
+    deleteUser
+}
--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const { json } = require('body-parser');
 
+const ctrl = require('./ctrl')
+
 const app = express();
 app.use(cors());
 app.use(json());
 
 const PORT = 1739;
 
-
+app.get('/getUsers', ctrl.getUsers);
+app.put('/updateUser', ctrl.updateUser);
+app.post('/createUser', ctrl.createUser);
+app.delete('/deleteUser', ctrl.deleteUser);
 
 
 
